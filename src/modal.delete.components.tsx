a5e38bd8163e25aa2a47458948ce711fe65e26af
pastorvelociraptor <circuncisaofabio@icloud.com>
2024-01-26 11:31:26 -0300

    feat: modal delete

---
+++ src/modal.delete.components.tsx
@@ -0,0 +1,42 @@
+import React from 'react';
+import styled from 'styled-components';
+
+const DivGrid = styled.div`
+    display: Grid;
+    width:100%;
+    height: 100px;
+`;
+const Form = styled.form`
+    display: Grid;
+    width:200px;
+    height: 100px;
+`;
+const Input = styled.input`
+width:100%;
+height: 100px;
+`;
+
+const Button = styled.button`
+width:100%;
+height: 100px;
+`;
+
+
+
+function ModalDeleteComponent() {
+  return (
+    <DivGrid>
+    <Form>
+        <label>Nome</label>
+        <Input></Input>
+        <label>Idade</label>
+        <Input></Input>
+        <label>Numero de telefone</label>
+        <Input></Input>
+        <Button>+</Button>
+    </Form>
+  </DivGrid>
+  );
+}
+
+export default ModalDeleteComponent;
