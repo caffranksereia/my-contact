faf8396088d9ccba6482c3457107af8038020499
pastorvelociraptor <circuncisaofabio@icloud.com>
2024-01-26 08:17:33 -0300

    feat: search contact

---
+++ src/search.contact.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+function SearchContact() {
+  return (
+    <div>
+      <form>
+        <input ></input>
+      </form>
+      <button>Pesquisar</button>
+    </div>
+  );
+}
+
+export default SearchContact;
