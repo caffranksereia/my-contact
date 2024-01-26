f8d685a17c593e48e83b2f08fab781f60c307cfe
pastorvelociraptor <circuncisaofabio@icloud.com>
2024-01-26 11:32:58 -0300

    feat: RegisterContact

--- src/register.contact.tsx
+++ src/register.contact.tsx
@@ -1,10 +1,25 @@
 import React from 'react';
+import styled from 'styled-components';
 
+const DivGrid = styled.div`
+    display: flex;
+    width:80%;
+    height: 20px;
+    justify-content:flex-end;
+    margin-top:1px;
+`;
+
+const Button = styled.button`
+width:20px;
+height: 20px;
+background:#d9d9d;
+justify-content:center;
+`;
 function RegisterContact() {
   return (
-    <div>
-      <button>Cadastrar</button>
-    </div>
+    <DivGrid>
+      <Button>+</Button>
+    </DivGrid>
   );
 }
 export default RegisterContact;
