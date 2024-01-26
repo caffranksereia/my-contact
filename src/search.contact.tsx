f93a7fab2d706668f3055a8d428d728ac444b330
pastorvelociraptor <circuncisaofabio@icloud.com>
2024-01-26 11:32:45 -0300

    feat: SearchContact component

--- src/search.contact.tsx
+++ src/search.contact.tsx
@@ -1,13 +1,45 @@
 import React from 'react';
+import styled from 'styled-components';
+
+const DivGrid = styled.div`
+    display: flex;
+    width:100%;
+    height: 100px;
+`;
+const Form = styled.form`
+    display: flex;
+    width:70%;
+    height: 50px;
+    margin: 10px;10px;5px;5px
+`;
+const Input = styled.input`
+width:75%;
+height: 45px;
+margin: 1px;1px;1px;1px;
+`;
+
+const Button = styled.button`
+width:70px;
+height: 45px;
+margin: 1px;1px;1px;1px;
+`;
+const Title = styled.h1`
+    font-color = 00000;
+    margin: 10px;25px;5px;5px
+`;
 
 function SearchContact() {
   return (
-    <div>
-      <form>
-        <input ></input>
-      </form>
-      <button>Pesquisar</button>
-    </div>
+    <DivGrid>
+       <Title>
+        My Contacts
+       </Title>
+      <Form>
+        <Input ></Input>
+        <Button>Pesquisar</Button>
+      </Form>
+     
+    </DivGrid>
   );
 }
 
