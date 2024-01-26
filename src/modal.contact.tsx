e06cf8df4475fba3cc82b1a34e99a226cf4bbbfa
pastorvelociraptor <circuncisaofabio@icloud.com>
2024-01-26 08:16:49 -0300

    feat: modal adiciona o contato

---
+++ src/modal.contact.tsx
@@ -0,0 +1,19 @@
+import React from 'react';
+
+function ModalContact() {
+  return (
+    <div>
+    <form>
+        <label>Nome</label>
+        <input></input>
+        <label>Idade</label>
+        <input></input>
+        <label>Numero de telefone</label>
+        <input></input>
+        <button>+</button>
+    </form>
+  </div>
+  );
+}
+
+export default ModalContact;
